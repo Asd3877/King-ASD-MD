6ffab38398d84aeaba296c2b90ddcc1d9523be0e
Asd3877 <Achinthashehan992@gmail.com>
2025-01-07 20:09:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/900435c6d3157c98c3c88.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello Im 𝐊𝐈𝐍𝐆 𝐀𝐒𝐃 𝐌𝐃 𝐖𝐀 𝐁𝐎𝐓 IM ALIVE now Thanks for asking.!",
 };
